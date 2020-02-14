7972117f944c6ee7bba9639dfa5125c529d359e0
Andrey.Grebenkov <Andrey.Grebenkov@nexign-systems.com>
2020-02-14 17:10:52 +0300

    Base app.

---
+++ src/main/java/com/portfolio/Constants.java
@@ -0,0 +1,8 @@
+package com.portfolio;
+
+/**
+ * Created by Grebenkov.Andrey on 14.10.2019
+ */
+public class Constants {
+    public static final String COMMA = ",";
+}
---
+++ src/main/java/com/portfolio/PortfolioInfoApp.java
@@ -0,0 +1,14 @@
+package com.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Grebenkov.Andrey
+ */
+@SpringBootApplication
+public class PortfolioInfoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioInfoApp.class, args);
+    }
+}
